7d6ab7f2c8c75255c0c59138f11ee567f11c3bc0
Sylvester Abreu <sylvester.abreu@googlemail.com>
2014-12-12 11:42:36 +0000

    StringUtils.split(null,"=")

--- src/test/StringUtilsTest.java
+++ src/test/StringUtilsTest.java
@@ -1,6 +1,8 @@
 import org.apache.commons.lang.StringUtils;
+import org.hamcrest.core.Is;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,5 +11,10 @@ public class StringUtilsTest {
     @Test
     public void assertThatStringUtilsContainsEqualInNull_isFalse() throws Exception {
         assertThat(StringUtils.contains(null, "="), is(false));
+    }
+
+    @Test
+    public void assertThatStringUtilsSplitEqualInNull_isNull() throws Exception {
+        assertThat(StringUtils.split(null, "="), is(equalTo(null)));
     }
 }
