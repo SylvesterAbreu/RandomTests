72efba5dd38531bb2521fe253206b779951c254d
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-03-13 14:45:56 +0000

    Organizing imports for CollectionUtilsTest

---
+++ src/test/CollectionUtilsTest.java
@@ -0,0 +1,22 @@
+import org.apache.commons.collections4.CollectionUtils;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class CollectionUtilsTest {
+
+
+    @Test
+    public void assertThatListCanNotHaveNullReferences() throws Exception {
+
+        List<String> nullReferencesCollection = new ArrayList<>();
+        nullReferencesCollection.add(null);
+        nullReferencesCollection.add(null);
+
+        assertTrue(CollectionUtils.isNotEmpty(nullReferencesCollection));
+    }
+}
