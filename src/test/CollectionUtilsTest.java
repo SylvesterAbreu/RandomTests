6de26175259fa4f5164f81c7c3dc14ea158fd22c
Sylvester Abreu <sylvester.abreu@digitaslbi.com>
2015-03-13 14:59:23 +0000

    Asserting that new reference refers to null value from collection

--- src/test/CollectionUtilsTest.java
+++ src/test/CollectionUtilsTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 
@@ -18,5 +20,15 @@ public class CollectionUtilsTest {
         nullReferencesCollection.add(null);
 
         assertTrue(nullReferencesCollection.contains(null));
+    }
+    
+    @Test
+    public void assertThatNewReferenceRefersToReferenceFromCollection () throws Exception {
+        
+        List <String> nullReferencesCollection = new ArrayList<>();
+        nullReferencesCollection.add(null);
+        final String nullReference = nullReferencesCollection.get(0);
+        
+        assertNull(nullReference);
     }
 }
